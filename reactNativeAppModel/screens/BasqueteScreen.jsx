b34bce75f81a266f2d52a52b5da4a79a42802f6e
nosepato <felipe.pinhoramos@outlook.com>
2024-04-17 21:39:00 -0300

    atualização do quiz 1.0

--- reactNativeAppModel/screens/BasqueteScreen.jsx
+++ reactNativeAppModel/screens/BasqueteScreen.jsx
@@ -1,10 +1,69 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
 import React from 'react'
+import { ScrollView } from 'react-native-web'
 
 const BasqueteScreen = () => {
   return (
-    <View>
-      <Text>Teste</Text>
+    <View style={styles.container}>
+      <View>
+       <Text> QUIZ </Text>
+       </View>
+
+       <View>
+       <Text> 1º) Qual o maior campeão da NBA ? </Text>
+       </View>
+
+       <View>
+       <Button title='Golden State'/>
+       <Button title='Chicago Bulls'/>
+       <Button title='Boston Celtics'/>
+       <Button title='Lakers*'/>
+       </View>
+
+       <View>
+       <Text> 2º) Quem é o maior cestinha da história ? </Text>
+       </View>
+
+       <View>
+       <Button title='Michael Jordan'/>
+       <Button title='Lebron James*'/>
+       <Button title='Stephen Curry'/>
+       <Button title='Oscar Schmidt'/>
+       </View>
+
+       <View>
+       <Text> 3º) Quem é o maior cestinha de 3 pontos da história ? </Text>
+       </View>
+
+       <View>
+       <Button title='Michael Jordan'/>
+       <Button title='Lebron James'/>
+       <Button title='Stephen Curry*'/>
+       <Button title='Kobe Bryant'/>
+       </View>
+
+       <View>
+       <Text> 4º) Em quantas partes é dividido uma partida de basquete ? </Text>
+       </View>
+
+       <View>
+       <Button title='5'/>
+       <Button title='2'/>
+       <Button title='3'/>
+       <Button title='4*'/>
+       </View>
+
+       <View>
+       <Text> 5º) Com quantas faltas individuais o jogador é expulso ? </Text>
+       </View>
+
+       <View>
+       <Button title='2'/>
+       <Button title='5'/>
+       <Button title='6*'/>
+       <Button title='10'/>
+       </View>
+
     </View>
   )
 }
--- reactNativeAppModel/screens/FutebolScreen.jsx
+++ reactNativeAppModel/screens/FutebolScreen.jsx
@@ -1,14 +1,76 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
 import React from 'react'
+import { ScrollView } from 'react-native-web'
 
 const FutebolScreen = () => {
   return (
     <View style={styles.container}>
-       <Text>Teste</Text>
+      <View>
+       <Text> QUIZ </Text>
+       </View>
+
+       <View>
+       <Text> 1º) Qual o maior campeão da Uefa Champions League ? </Text>
+       </View>
+
+       <View>
+       <Button title='Bayer de Munique'/>
+       <Button title='Liverpool'/>
+       <Button title='Real Madrid*'/>
+       <Button title='Milan'/>
+       </View>
+
+       <View>
+       <Text> 2º) Qual o maior campeão da Copa do Mundo ? </Text>
+       </View>
+
+       <View>
+       <Button title='Argentina'/>
+       <Button title='Itália'/>
+       <Button title='Alemanha'/>
+       <Button title='Brasil*'/>
+       </View>
+
+       <View>
+       <Text> 3º) Qual o maior campeão da Libertadores ? </Text>
+       </View>
+
+       <View>
+       <Button title='Boca Juniors'/>
+       <Button title='Palmeiras'/>
+       <Button title='River Plate'/>
+       <Button title='Independiente*'/>
+       </View>
+
+       <View>
+       <Text> 4º) Quantos jogadores cada time podem ter em campo ? </Text>
+       </View>
+
+       <View>
+       <Button title='11*'/>
+       <Button title='10'/>
+       <Button title='22'/>
+       <Button title='12'/>
+       </View>
+
+       <View>
+       <Text> 5º) Com quantos cartões amarelos o jogador é expulso de campo ? </Text>
+       </View>
+
+       <View>
+       <Button title='4'/>
+       <Button title='2*'/>
+       <Button title='3'/>
+       <Button title='1'/>
+       </View>
+
     </View>
+    
   )
 }
 
 export default FutebolScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+})
