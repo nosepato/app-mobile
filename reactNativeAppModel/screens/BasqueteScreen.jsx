1f66df85b17a4469a94c209ff9bbdd1a156ec4e6
nosepato <felipe.pinhoramos@outlook.com>
2024-04-24 16:47:20 -0300

    last update 1.0

--- reactNativeAppModel/screens/BasqueteScreen.jsx
+++ reactNativeAppModel/screens/BasqueteScreen.jsx
@@ -1,30 +1,63 @@
 import { StyleSheet, Text, View, Button } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { ScrollView } from 'react-native-web'
 
 const BasqueteScreen = () => {
+  const [isCorrectAnswer1, setCorrectAnswer1] = useState(false)
+  const [isIncorrectAnswer1_1, setIncorrectAnswer1_1] = useState(false)
+  const [isIncorrectAnswer1_2, setIncorrectAnswer1_2] = useState(false)
+  const [isIncorrectAnswer1_3, setIncorrectAnswer1_3] = useState(false)
+  const [isCorrectAnswer2, setCorrectAnswer2] = useState(false)
+  const [isIncorrectAnswer2_1, setIncorrectAnswer2_1] = useState(false)
+  const [isIncorrectAnswer2_2, setIncorrectAnswer2_2] = useState(false)
+  const [isIncorrectAnswer2_3, setIncorrectAnswer2_3] = useState(false)
+  const [isCorrectAnswer3, setCorrectAnswer3] = useState(false)
+  const [isIncorrectAnswer3_1, setIncorrectAnswer3_1] = useState(false)
+  const [isIncorrectAnswer3_2, setIncorrectAnswer3_2] = useState(false)
+  const [isIncorrectAnswer3_3, setIncorrectAnswer3_3] = useState(false)
+  const [isCorrectAnswer4, setCorrectAnswer4] = useState(false)
+  const [isIncorrectAnswer4_1, setIncorrectAnswer4_1] = useState(false)
+  const [isIncorrectAnswer4_2, setIncorrectAnswer4_2] = useState(false)
+  const [isIncorrectAnswer4_3, setIncorrectAnswer4_3] = useState(false)
+  const [isCorrectAnswer5, setCorrectAnswer5] = useState(false)
+  const [isIncorrectAnswer5_1, setIncorrectAnswer5_1] = useState(false)
+  const [isIncorrectAnswer5_2, setIncorrectAnswer5_2] = useState(false)
+  const [isIncorrectAnswer5_3, setIncorrectAnswer5_3] = useState(false)
+
+  const [correctAnswersCount, setCorrectAnswersCount] = useState(0);
+  const [incorrectAnswersCount, setIncorrectAnswersCount] = useState(0);
+
+  const handleAnswer = (isCorrect) => {
+    if (isCorrect) {
+      setCorrectAnswersCount(prevCount => prevCount + 1);
+    } else {
+      setIncorrectAnswersCount(prevCount => prevCount + 1);
+    }
+  }
+
   return (
     <ScrollView>
     <View style={styles.container}>
+
        <View>
        <Text style={styles.text}> 1º) Qual o maior campeão da NBA ? </Text>
        </View>
 
        <View style={styles.buttonContainer}>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='Golden State'/>
+       <Button title='Golden State' color= {isIncorrectAnswer1_1 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer1_1(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='Chicago Bulls'/>
+       <Button title='Chicago Bulls' color= {isIncorrectAnswer1_2 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer1_2(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='Boston Celtics'/>
+       <Button title='Boston Celtics' color= {isIncorrectAnswer1_3 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer1_3(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='LA Lakers*'/>
+       <Button title='LA Lakers' color= {isCorrectAnswer1 ? 'green' : 'blue'} onPress={() => {setCorrectAnswer1(true); handleAnswer(true)}}/>
        </View>
        </View>
 
@@ -34,19 +67,19 @@ const BasqueteScreen = () => {
 
        <View style={styles.buttonContainer}>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='Michael Jordan'/>
+       <Button title='Michael Jordan' color= {isIncorrectAnswer2_1 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer2_1(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='Lebron James*'/>
+       <Button title='Lebron James' color= {isCorrectAnswer2 ? 'green' : 'blue'} onPress={() => {setCorrectAnswer2(true); handleAnswer(true)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='Stephen Curry'/>
+       <Button title='Stephen Curry' color= {isIncorrectAnswer2_2 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer2_2(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='Oscar Schmidt'/>
+       <Button title='Oscar Schmidt' color= {isIncorrectAnswer2_3 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer2_3(true); handleAnswer(false)}}/>
        </View>
        </View>
 
@@ -56,19 +89,19 @@ const BasqueteScreen = () => {
 
        <View style={styles.buttonContainer}>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='Michael Jordan'/>
+       <Button title='Michael Jordan' color= {isIncorrectAnswer3_1 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer3_1(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='Lebron James'/>
+       <Button title='Lebron James' color= {isIncorrectAnswer3_2 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer3_2(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='Stephen Curry*'/>
+       <Button title='Stephen Curry' color= {isCorrectAnswer3 ? 'green' : 'blue'} onPress={() => {setCorrectAnswer3(true); handleAnswer(true)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='Kobe Bryant'/>
+       <Button title='Kobe Bryant' color= {isIncorrectAnswer3_3 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer3_3(true); handleAnswer(false)}}/>
        </View>
        </View>
 
@@ -77,19 +110,19 @@ const BasqueteScreen = () => {
        </View>
        <View style={styles.buttonContainer}>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='5'/>
+       <Button title='5' color= {isIncorrectAnswer4_1 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer4_1(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='2'/>
+       <Button title='2' color= {isIncorrectAnswer4_2 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer4_2(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='3'/>
+       <Button title='3' color= {isIncorrectAnswer4_3 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer4_3(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='4*'/>
+       <Button title='4' color= {isCorrectAnswer4 ? 'green' : 'blue'} onPress={() => {setCorrectAnswer4(true); handleAnswer(true)}}/>
        </View>
        </View>
 
@@ -99,28 +132,33 @@ const BasqueteScreen = () => {
 
        <View style={styles.buttonContainer}>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='2'/>
+       <Button title='2' color= {isIncorrectAnswer5_1 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer5_1(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='5'/>
+       <Button title='5' color= {isIncorrectAnswer5_2 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer5_2(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='6*'/>
+       <Button title='6' color= {isCorrectAnswer5 ? 'green' : 'blue'} onPress={() => {setCorrectAnswer5(true); handleAnswer(true)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='10'/>
+       <Button title='10' color= {isIncorrectAnswer5_3 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer5_3(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space2}></View>
 
        </View>
 
+       <View style={styles.resultsContainer}>
+          <Text style={styles.resultsText}>Respostas Certas: {correctAnswersCount}</Text>
+          <Text style={styles.resultsText}>Respostas Erradas: {incorrectAnswersCount}</Text>
+       </View>
+
+       <View style={styles.space}></View>
+
        <View style={[styles.viewButtonFinish, {flex:1}]}>
-        <Button
-          title='Finalizar'
-          onPress={() => {navigation.navigate("home")}}/>
+        <Button title='Finalizar' onPress={() => {navigation.navigate("home")}}/>
        </View>
 
     </View>
@@ -131,35 +169,43 @@ const BasqueteScreen = () => {
 export default BasqueteScreen
 
 const styles = StyleSheet.create({
-container: {
-    flex: 1,
-    alignItems: 'left',
-    padding: 20,
-    backgroundColor: 'black'
-},
-text: {
-    fontSize: '1.3em',
-    textAlign: 'left',
-    color: 'white'
-},
-buttonContainer: {
-    alignItems: 'left',
-    padding: 20
-},
-viewButton: {
-    width: 150,
-    borderRadius: 15, 
-    overflow: 'hidden'
-},
-viewButtonFinish: {
-    alignItems: 'center',
-    borderRadius: 15,
-    overflow: 'hidden'
-},
-space: {
-    height: 10
-},
-space2: {
-    height: 50
-}
+    container: {
+        flex: 1,
+        alignItems: 'left',
+        padding: 20,
+        backgroundColor: 'black'
+    },
+    text: {
+        fontSize: '1.3em',
+        textAlign: 'left',
+        color: 'white'
+    },
+    buttonContainer: {
+        alignItems: 'left',
+        padding: 20
+    },
+    viewButton: {
+        width: 150,
+        borderRadius: 15, 
+        overflow: 'hidden'
+    },
+    viewButtonFinish: {
+      alignItems: 'center',
+      borderRadius: 15,
+      overflow: 'hidden'
+    },
+    space: {
+        height: 10
+    },
+    space2: {
+      height: 50
+    },
+    resultsContainer: {
+      marginTop: 20,
+      alignItems: 'center'
+    },
+    resultsText: {
+      fontSize: 20,
+      color: 'white'
+    }
 })
--- reactNativeAppModel/screens/FutebolScreen.jsx
+++ reactNativeAppModel/screens/FutebolScreen.jsx
@@ -1,11 +1,40 @@
 import { StyleSheet, Text, View, Button } from 'react-native'
 import React, { useState } from 'react'
-import { ImageBackground, Modal, ScrollView } from 'react-native-web'
+import { ScrollView } from 'react-native-web'
 
 const FutebolScreen = () => {
-  const [isCorrectAnswer, setCorrectAnswer] = useState(false)
-  const [isIncorrectAnswer, setIncorrectAnswer] = useState(false)
-  
+  const [isCorrectAnswer1, setCorrectAnswer1] = useState(false)
+  const [isIncorrectAnswer1_1, setIncorrectAnswer1_1] = useState(false)
+  const [isIncorrectAnswer1_2, setIncorrectAnswer1_2] = useState(false)
+  const [isIncorrectAnswer1_3, setIncorrectAnswer1_3] = useState(false)
+  const [isCorrectAnswer2, setCorrectAnswer2] = useState(false)
+  const [isIncorrectAnswer2_1, setIncorrectAnswer2_1] = useState(false)
+  const [isIncorrectAnswer2_2, setIncorrectAnswer2_2] = useState(false)
+  const [isIncorrectAnswer2_3, setIncorrectAnswer2_3] = useState(false)
+  const [isCorrectAnswer3, setCorrectAnswer3] = useState(false)
+  const [isIncorrectAnswer3_1, setIncorrectAnswer3_1] = useState(false)
+  const [isIncorrectAnswer3_2, setIncorrectAnswer3_2] = useState(false)
+  const [isIncorrectAnswer3_3, setIncorrectAnswer3_3] = useState(false)
+  const [isCorrectAnswer4, setCorrectAnswer4] = useState(false)
+  const [isIncorrectAnswer4_1, setIncorrectAnswer4_1] = useState(false)
+  const [isIncorrectAnswer4_2, setIncorrectAnswer4_2] = useState(false)
+  const [isIncorrectAnswer4_3, setIncorrectAnswer4_3] = useState(false)
+  const [isCorrectAnswer5, setCorrectAnswer5] = useState(false)
+  const [isIncorrectAnswer5_1, setIncorrectAnswer5_1] = useState(false)
+  const [isIncorrectAnswer5_2, setIncorrectAnswer5_2] = useState(false)
+  const [isIncorrectAnswer5_3, setIncorrectAnswer5_3] = useState(false)
+
+  const [correctAnswersCount, setCorrectAnswersCount] = useState(0);
+  const [incorrectAnswersCount, setIncorrectAnswersCount] = useState(0);
+
+  const handleAnswer = (isCorrect) => {
+    if (isCorrect) {
+      setCorrectAnswersCount(prevCount => prevCount + 1);
+    } else {
+      setIncorrectAnswersCount(prevCount => prevCount + 1);
+    }
+  }
+
   return (
     <ScrollView>
     <View style={styles.container}>
@@ -15,28 +44,22 @@ const FutebolScreen = () => {
        </View>
 
        <View style={styles.buttonContainer}>
-       <View style={[styles.viewButton, {flex: 1}]}>
-       <Button color= {isIncorrectAnswer ? 'red' : 'blue'}title='Bayern Munich' style={styles.button} onPress={() => {
-        
-        setIncorrectAnswer(true)
-       }}/>
-       </View>
-       <View style={styles.space}></View>
-       <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='Liverpool'/>
-       </View>
-       <View style={styles.space}></View>
-       <View style={[styles.viewButton, {flex: 1}]}>
-       <Button color= {isCorrectAnswer ? 'green' : 'blue'}title='Real Madrid' onPress={() => {
-        
-        setCorrectAnswer(true)
-       }}/>
-       </View>
-       <View style={styles.space}></View>
-       <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title=' AC Milan'/>
-       </View>
-       </View>
+          <View style={[styles.viewButton, {flex: 1}]}>
+            <Button title='Bayern Munich' color= {isIncorrectAnswer1_1 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer1_1(true); handleAnswer(false)}}/>
+          </View>
+          <View style={styles.space}></View>
+          <View style={[styles.viewButton, {flex: 1}]}>
+            <Button title='Liverpool' color= {isIncorrectAnswer1_2 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer1_2(true); handleAnswer(false)}}/>
+          </View>
+          <View style={styles.space}></View>
+          <View style={[styles.viewButton, {flex: 1}]}>
+            <Button title='Real Madrid' color= {isCorrectAnswer1 ? 'green' : 'blue'} onPress={() => {setCorrectAnswer1(true); handleAnswer(true)}}/>
+          </View>
+          <View style={styles.space}></View>
+          <View style={[styles.viewButton, {flex: 1}]}>
+            <Button title=' AC Milan' color= {isIncorrectAnswer1_3 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer1_3(true); handleAnswer(false)}}/>
+          </View>
+        </View>
 
        <View>
        <Text style={styles.text}> 2º) Qual o maior campeão da Copa do Mundo ? </Text>
@@ -44,19 +67,19 @@ const FutebolScreen = () => {
 
        <View style={styles.buttonContainer}>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='Argentina'/>
+       <Button title='Argentina' color= {isIncorrectAnswer2_1 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer2_1(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='Itália'/>
+       <Button title='Itália' color= {isIncorrectAnswer2_2 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer2_2(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='Alemanha*'/>
+       <Button title='Alemanha' color= {isIncorrectAnswer2_3 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer2_3(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='Brasil*'/>
+       <Button title='Brasil' color= {isCorrectAnswer2 ? 'green' : 'blue'} onPress={() => {setCorrectAnswer2(true); handleAnswer(true)}}/>
        </View>
        </View>
 
@@ -66,19 +89,19 @@ const FutebolScreen = () => {
 
        <View style={styles.buttonContainer}>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='Boca Juniors'/>
+       <Button title='Boca Juniors' color= {isIncorrectAnswer3_1 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer3_1(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='Palmeiras'/>
+       <Button title='Palmeiras' color= {isIncorrectAnswer3_2 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer3_2(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='River Plate'/>
+       <Button title='River Plate' color= {isIncorrectAnswer3_3 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer3_3(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='Independiente*'/>
+       <Button title='Independiente' color= {isCorrectAnswer3 ? 'green' : 'blue'} onPress={() => {setCorrectAnswer3(true); handleAnswer(true)}}/>
        </View>
        </View>
 
@@ -88,19 +111,19 @@ const FutebolScreen = () => {
 
        <View style={styles.buttonContainer}>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='11*'/>
+       <Button title='11' color= {isCorrectAnswer4 ? 'green' : 'blue'} onPress={() => {setCorrectAnswer4(true); handleAnswer(true)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='10'/>
+       <Button title='10' color= {isIncorrectAnswer4_1 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer4_1(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='22'/>
+       <Button title='22' color= {isIncorrectAnswer4_2 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer4_2(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='12'/>
+       <Button title='12' color= {isIncorrectAnswer4_3 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer4_3(true); handleAnswer(false)}}/>
        </View>
        </View>
 
@@ -110,28 +133,34 @@ const FutebolScreen = () => {
       
        <View style={styles.buttonContainer}>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='4'/>
+       <Button title='4' color= {isIncorrectAnswer5_1 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer5_1(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='2*'/>
+       <Button title='2' color= {isCorrectAnswer5 ? 'green' : 'blue'} onPress={() => {setCorrectAnswer5(true); handleAnswer(true)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='3'/>
+       <Button title='3' color= {isIncorrectAnswer5_2 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer5_2(true); handleAnswer(false)}}/>
        </View>
        <View style={styles.space}></View>
        <View style={[styles.viewButton, {flex: 1}]}>
-       <Button title='1'/>
+       <Button title='1' color= {isIncorrectAnswer5_3 ? 'red' : 'blue'} onPress={() => {setIncorrectAnswer5_3(true); handleAnswer(false)}}/>
        </View>
+
        <View style={styles.space2}></View>
       
        </View>
 
+       <View style={styles.resultsContainer}>
+          <Text style={styles.resultsText}>Respostas Certas: {correctAnswersCount}</Text>
+          <Text style={styles.resultsText}>Respostas Erradas: {incorrectAnswersCount}</Text>
+       </View>
+
+       <View style={styles.space}></View>
+
        <View style={[styles.viewButtonFinish, {flex:1}]}>
-        <Button
-          title='Finalizar'
-          onPress={() => {navigation.navigate("home")}}/>
+        <Button title='Finalizar' onPress={() => {navigation.navigate("home")}}/>
        </View>
 
     </View>
@@ -173,7 +202,12 @@ space: {
 space2: {
   height: 50
 },
-button:{
-  color: '#000000'
+resultsContainer: {
+  marginTop: 20,
+  alignItems: 'center'
+},
+resultsText: {
+  fontSize: 20,
+  color: 'white'
 }
 })
